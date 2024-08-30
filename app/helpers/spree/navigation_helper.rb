102b34e56100779e5e0664e0e736b1d50ebbb364
Alejandro Sanchez <apple@Apples-MacBook-Pro.local>
2024-08-30 15:43:57 -0500

    update configuration

--- app/helpers/spree/navigation_helper.rb
+++ app/helpers/spree/navigation_helper.rb
@@ -1,18 +1,22 @@
 # app/helpers/spree/navigation_helper.rb
 module Spree
   module NavigationHelper
-    # Helper para crear el árbol de menú principal
     def main_menu_tree(name, icon: nil, sub_menu: nil, url: '#')
       content_tag(:li, class: 'nav-item') do
-        link_to url, class: 'nav-link', 'data-toggle': 'collapse', 'aria-expanded': 'false' do
+        link_to(url, class: 'nav-link', 'data-toggle': 'collapse', 'aria-expanded': 'false') do
+          # Agrega el ícono y el texto del menú principal
           concat(content_tag(:i, '', class: "nav-icon #{icon}")) if icon
           concat(content_tag(:p) do
             concat name
-            concat(content_tag(:i, '', class: 'right fas fa-angle-left'))
+            concat content_tag(:i, '', class: 'right fas fa-angle-left')
           end)
         end +
+
+        # Genera el submenú (lista de navegación secundaria)
         content_tag(:ul, class: "nav nav-treeview collapse", id: sub_menu) do
           # Aquí puedes agregar los elementos secundarios del menú
+          # Puedes iterar sobre un array de elementos o agregar contenido manualmente
+          ''
         end
       end
     end
