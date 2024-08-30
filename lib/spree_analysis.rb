9aec14b0472a9be757cb2ecc773dff25298abc7c
Alejandro Sanchez <apple@Apples-MacBook-Pro.local>
2024-08-30 14:21:43 -0500

    update configuration

--- lib/spree_analysis.rb
+++ lib/spree_analysis.rb
@@ -1,5 +1,6 @@
 require 'spree_core'
 require 'spree_extension'
+require 'spree_analysis'
 require 'spree_analysis/configuration'
 require 'spree_analysis/engine'
 require 'spree_analysis/version'
@@ -8,6 +9,10 @@ module SpreeAnalysis
   class ReportConfig
     def self.configure
       yield configuration if block_given?
+    end
+
+    def self.configuration
+      @config ||= Spree::Report::Configuration.new
     end
   end
 end
