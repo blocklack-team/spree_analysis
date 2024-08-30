f869eed0a4335b8f37069f90de15a9db1a0aeefe
Alejandro Sanchez <apple@Apples-MacBook-Pro.local>
2024-08-30 14:45:48 -0500

    update configuration

--- lib/spree_analysis.rb
+++ lib/spree_analysis.rb
@@ -1,6 +1,6 @@
 require 'spree_core'
 require 'spree_extension'
-require './../app'
+require 'spree'
 require 'spree_analysis/configuration'
 require 'spree_analysis/engine'
 require 'spree_analysis/version'
